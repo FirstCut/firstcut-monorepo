a961d6ef5919997cc16e9164ffedd1ab8c91f25d
Lucy Richards <lucyannerichards@gmail.com>
2018-10-01 10:37:26 -0700

    testing clean

---
+++ packages/firstcut-utils/index.spec.js
@@ -0,0 +1,13 @@
+
+import { pluralize } from 'src/index.js';
+
+test('should pluralize by appending s in correct cases', () => {
+  expect(pluralize('project')).toBe('projects');
+  expect(pluralize('card')).toBe('cards');
+  expect(pluralize('care')).toBe('cares');
+  expect(pluralize('company')).not.toBe('companys');
+});
+test('should pluralize by changing y to ies in correct cases', () => {
+  expect(pluralize('company')).toBe('companies');
+  expect(pluralize('sky')).toBe('skies');
+});
